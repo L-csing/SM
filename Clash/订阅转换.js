ca81ea8168b3eec215e8816b14dc530b2f43e85a
L-csing <157201870+L-csing@users.noreply.github.com>
2024-11-01 16:29:36 +0800

    Update 订阅转换.js

--- Clash/订阅转换.js
+++ Clash/订阅转换.js
@@ -8,7 +8,7 @@ const ruleProviderCommon = {
 // 策略组通用配置
 const groupBaseOption = {
   "interval": 300,
-  "url": "http://connectivitycheck.gstatic.com/generate_204",
+  "url": "http://1.1.1.1/generate_204",
   "max-failed-times": 3,
 };
 
